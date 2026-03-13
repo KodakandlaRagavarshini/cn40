dd6e7e2b179892e6dc8edd87fc71a1190a293552
KodakandlaRagavarshini <ragakodakandla@gmail.com>
2026-03-13 12:37:27 +0530

    first commit

---
+++ routes/studentRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const Student = require("../models/Student");
+
+
+//CREATE
+router.post("/students" , async(req,res) => {
+    const student = new Student(req.body);
+    await student.save();
+    res.send(student);
+});
+
+//READ
+router.get("/students",async(req,res) => {
+    const students = await Student.find();
+    res.send(Student);
+});
+
+module.exports = router;
